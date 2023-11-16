2efb4b1732e43aa5004bc7dd4acf97ba44991e59
Jose777 <jperello@a.iescanpeublanc.cat>
2023-11-16 11:19:04 +0100

    Views.py

--- aplicación/views.py
+++ aplicación/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Persona, Visitas
 
-# Create your views here.
+def lista_alumnos(request):
+    pass
+
+def detalle_alumno(request, num_exp):
+    pass
+
+def nueva_visita(request):
+    pass
+
+def lista_visitas(request):
+    pass
