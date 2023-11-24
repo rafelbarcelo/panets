83696dd38cec605706a531a1064f5eefc41e4856
adriagarciaolmo <agarcia@a.iescanpeublanc.cat>
2023-11-24 09:48:46 +0100

    jyyf

--- aplicación/forms.py
+++ aplicación/forms.py
@@ -2,6 +2,6 @@ from django imports forms
 
 class Contacto(forms.Form)
     nombre=forms.CharField(label="Nombre", required=True)
-    email= forms.EmailField(label="Email", required=True)
+    email=forms.EmailField(label="Email", required=True)
     contenido=forms.CharField(label="Contenido"
     required=True, widget=forms.Textarea)
