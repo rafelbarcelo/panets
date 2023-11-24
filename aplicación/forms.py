6396afc663f1dae57bbead682312bcf1678b8342
Rafel_Barcelo <rbarcelo@a.iescanpeublanc.cat>
2023-11-24 09:44:15 +0100

    forms arreglat

--- aplicación/forms.py
+++ aplicación/forms.py
@@ -1,7 +1,7 @@
-from django imports forms
+from django import forms
 
-class Contacto(forms.Form)
+class Contacto(forms.Form):
     nombre=forms.CharField(label="Nombre", required=True)
-    email= forms.EmailField(label="Email", required=True)
-    contenido=forms.CharField(label="Contenido"
+    email=forms.EmailField(label="Email", required=True)
+    contenido=forms.CharField(label="Contenido",
     required=True, widget=forms.Textarea)
