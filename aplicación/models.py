f9379b10243e883ff8a124be155048cca199557a
Rafel_Barcelo <rbarcelo@a.iescanpeublanc.cat>
2023-11-30 11:21:23 +0100

    Voy a hacer pull de views

--- aplicación/models.py
+++ aplicación/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
 class Tipo(models.Model):
-    nombre=models.Charfield(max_lenght=50)
+    nombre=models.CharField(max_length=50)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name="TipoProducto"
-        verbose_name_plural="TiposProductos"
-
-    def__str__(self):
-        return self.nombre
+class Meta:
+    verbose_name="TipoProducto"
+    verbose_name_plural="TiposProductos"
 
 class Producto (models.Model):
-    nombre=models.Charfield(max_lenght=50)
+    nombre=models.CharField(max_length=50)
     tipo=models.ForeignKey(Tipo, on_delete=models.CASCADE)
-    imagen=models.ImageField(upload_to="gimnasio", null=True, balnk=True)
+    imagen=models.ImageField(upload_to="gimnasio", null=True, blank=True)
     precio=models.FloatField()
     stock=models.BooleanField(default=True)
     created=models.DateTimeField(auto_now_add=True)
