4a35bf0b69fd438f05881b4d0838d273766959e7
Rafel_Barcelo <rbarcelo@a.iescanpeublanc.cat>
2023-12-14 11:19:54 +0100

    sdkfjdsb

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path  
-=======
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
    
->>>>>>> c37b807f2b396e65f8a6863716c6721f6a1a8b38
